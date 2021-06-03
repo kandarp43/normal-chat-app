cb88d02100cbe1fce18f8b9c258112eb4f48f57c
kandarp43 <kandarpdangi@gmail.com>
2021-06-03 11:43:49 +0530

    init dashboard

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react'
+import Dashboard from './Components/Dashboard'
 import Login from './Components/Login'
 import useLocalstorage from './Hooks/useLocalstorage'
 
 function App() {
   const [id, setId] = useLocalstorage('id')
-  return (
-    <>
-      {id}
-      <Login onIdSubmit={setId} />
-    </>
-  )
+  return id ? <Dashboard id={id} /> : <Login onIdSubmit={setId} />
 }
 
 export default App
---
+++ src/Components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dashboard({id}) {
+    return (
+        <div>
+            {id}
+        </div>
+    )
+}
+
+export default Dashboard
