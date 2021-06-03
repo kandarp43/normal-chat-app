4eff1b553ebdefc0dcd248f19b61f4a1cacb0348
kandarp43 <kandarpdangi@gmail.com>
2021-06-03 11:50:44 +0530

    init

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,11 +1,8 @@
 import React from 'react'
+import Sidebar from './Sidebar'
 
-function Dashboard({id}) {
-    return (
-        <div>
-            {id}
-        </div>
-    )
+function Dashboard({ id }) {
+  return <Sidebar id={id} />
 }
 
 export default Dashboard
---
+++ src/Components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidebar({id}) {
+    return (
+        <div>
+            sidebar
+        </div>
+    )
+}
+
+export default Sidebar
