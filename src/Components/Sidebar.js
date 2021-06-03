9994c315af9c0106cc09420c79cfeb3144c39f16
kandarp43 <kandarpdangi@gmail.com>
2021-06-03 12:17:51 +0530

    init

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Nav, Tab } from 'react-bootstrap'
 
-function Sidebar({id}) {
-    return (
-        <div>
-            sidebar
-        </div>
-    )
+function Sidebar({ id }) {
+  return (
+    <div style={{ width: '250px' }} className='d-flex flex-column'>
+      <Tab.Container>
+        <Nav variant='tabs' className='justify-content-center'>
+          <Nav.Item>
+            <Nav.Link eventKey='conversations'>Conversations</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey='contacts'>Contacts</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Tab.Container>
+    </div>
+  )
 }
 
 export default Sidebar
