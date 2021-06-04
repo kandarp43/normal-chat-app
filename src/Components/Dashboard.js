829512193d8344c53db070e5bc261522876a6a59
kandarp43 <kandarpdangi@gmail.com>
2021-06-04 23:56:46 +0530

    init

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -3,7 +3,7 @@ import Sidebar from './Sidebar'
 
 function Dashboard({ id }) {
   return (
-    <div>
+    <div className='d-flex' style={{ height: '100vh' }}>
       <Sidebar id={id} />
     </div>
   )
