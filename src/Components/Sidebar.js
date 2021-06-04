50c8a3a08e8bc4518fac2fd8bc0e81d5014edc77
kandarp43 <kandarpdangi@gmail.com>
2021-06-04 23:58:53 +0530

    init

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -6,7 +6,7 @@ import Contacts from './Contacts'
 const CONVERSATIONS_KEY = 'conversations'
 const CONTACTS_KEY = 'contacts'
 
-function Sidebar() {
+function Sidebar({ id }) {
   const [activekey, setActivekey] = useState(CONVERSATIONS_KEY)
   return (
     <div style={{ width: '250px' }} className='d-flex flex-column'>
@@ -27,6 +27,7 @@ function Sidebar() {
             <Contacts />
           </Tab.Pane>
         </Tab.Content>
+        Your I'd : <span className='text-muted'>{id}</span>
       </Tab.Container>
     </div>
   )
