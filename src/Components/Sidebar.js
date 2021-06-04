490c2720190b77977deb526e92bcdfef9794b844
kandarp43 <kandarpdangi@gmail.com>
2021-06-04 22:49:15 +0530

    init

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Nav, Tab } from 'react-bootstrap'
 
 function Sidebar({ id }) {
+  const [activekey, setActivekey] = useState('conversations')
   return (
     <div style={{ width: '250px' }} className='d-flex flex-column'>
-      <Tab.Container>
+      <Tab.Container activeKey={activekey} onSelect={setActivekey}>
         <Nav variant='tabs' className='justify-content-center'>
           <Nav.Item>
             <Nav.Link eventKey='conversations'>Conversations</Nav.Link>
