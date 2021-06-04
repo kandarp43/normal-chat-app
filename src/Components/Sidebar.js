a957e059ae276918b349af2e77944ff0130288b6
kandarp43 <kandarpdangi@gmail.com>
2021-06-04 23:05:15 +0530

    init

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -3,24 +3,27 @@ import { Nav, Tab } from 'react-bootstrap'
 import Conversations from './Conversations'
 import Contacts from './Contacts'
 
+const CONVERSATIONS_KEY = 'conversations'
+const CONTACTS_KEY = 'contacts'
+
 function Sidebar({ id }) {
-  const [activekey, setActivekey] = useState('conversations')
+  const [activekey, setActivekey] = useState(CONVERSATIONS_KEY)
   return (
     <div style={{ width: '250px' }} className='d-flex flex-column'>
       <Tab.Container activeKey={activekey} onSelect={setActivekey}>
         <Nav variant='tabs' className='justify-content-center'>
           <Nav.Item>
-            <Nav.Link eventKey='conversations'>Conversations</Nav.Link>
+            <Nav.Link eventKey={CONVERSATIONS_KEY}>Conversations</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey='contacts'>Contacts</Nav.Link>
+            <Nav.Link eventKey={CONTACTS_KEY}>Contacts</Nav.Link>
           </Nav.Item>
         </Nav>
         <Tab.Content>
-          <Tab.Pane eventKey='conversations'>
+          <Tab.Pane eventKey={CONVERSATIONS_KEY}>
             <Conversations />
           </Tab.Pane>
-          <Tab.Pane eventKey='contacts'>
+          <Tab.Pane eventKey={CONTACTS_KEY}>
             <Contacts />
           </Tab.Pane>
         </Tab.Content>
