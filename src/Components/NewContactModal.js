e8343da0232f0705f1c1b4df84244217cf2d8ba7
kandarp43 <kandarpdangi@gmail.com>
2021-06-05 23:50:26 +0530

    init

--- src/Components/NewContactModal.js
+++ src/Components/NewContactModal.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Modal } from 'react-bootstrap'
 
 function NewContactModal({ closeModal }) {
-  return <Modal.Header close> create Contact</Modal.Header>
+  return <Modal.Header closebutton> create Contact</Modal.Header>
 }
 
 export default NewContactModal
