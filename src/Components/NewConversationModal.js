06e48d2db20a805d945469c0fa0dbf609f052e63
kandarp43 <kandarpdangi@gmail.com>
2021-06-06 23:29:25 +0530

    init

--- src/Components/NewConversationModal.js
+++ src/Components/NewConversationModal.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Form, Modal } from 'react-bootstrap'
 
 function NewConversationModal({ closeModal }) {
+
+    const idRef = useRef()
+    const nameRef = useRef()
   function handleSubmit(e) {
     e.preventDefault()
   }
@@ -11,8 +14,12 @@ function NewConversationModal({ closeModal }) {
       <Modal.Body>
         <Form onSubmit={handleSubmit}>
           <Form.Group>
-            <Form.Label></Form.Label>
-            <Form.Control></Form.Control>
+            <Form.Label>Id</Form.Label>
+            <Form.Control type='text' ref={idRef} required />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Name</Form.Label>
+            <Form.Control type='text' ref={nameRef} required />
           </Form.Group>
         </Form>
       </Modal.Body>
