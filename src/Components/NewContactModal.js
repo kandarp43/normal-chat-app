427a1b82a3a2f347faa1c4b22831a8eb30f1dffe
kandarp43 <kandarpdangi@gmail.com>
2021-06-06 23:25:01 +0530

    init

--- src/Components/NewContactModal.js
+++ src/Components/NewContactModal.js
@@ -14,11 +14,11 @@ function NewContactModal({ closeModal }) {
         <Form onSubmit={handleSubmit}>
           <Form.Group>
             <Form.Label>Id</Form.Label>
-            <Form.Control type='text' ref={idRef} required></Form.Control>
+            <Form.Control type='text' ref={idRef} required />
           </Form.Group>
           <Form.Group>
             <Form.Label>Name</Form.Label>
-            <Form.Control type='text' ref={nameRef} required></Form.Control>
+            <Form.Control type='text' ref={nameRef} required />
           </Form.Group>
         </Form>
       </Modal.Body>
