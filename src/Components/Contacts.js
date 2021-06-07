64bfb6d925d683e26e838e56c691e8ec84e497d7
kandarp43 <kandarpdangi@gmail.com>
2021-06-07 13:22:28 +0530

    init

--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { ListGroup } from 'react-bootstrap'
+import { useContacts } from '../Contexts/ContactsProvider'
 
 function Contacts() {
-  return <div>contacts</div>
+  const { contacts } = useContacts()
+  return (
+    <ListGroup variant='flush'>
+      {contacts.map((contact) => (
+        <ListGroup.Item key={contact.id}>{contact.name}</ListGroup.Item>
+      ))}
+    </ListGroup>
+  )
 }
 
 export default Contacts
