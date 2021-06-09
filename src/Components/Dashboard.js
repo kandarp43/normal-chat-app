00530718ff92d21f3d47a5ce09af2afeb687bdd5
kandarp43 <kandarpdangi@gmail.com>
2021-06-09 20:10:10 +0530

    init

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useConversations } from '../Contexts/ConversationsProvider'
 import OpenConversation from './OpenConversation'
 import Sidebar from './Sidebar'
 
 function Dashboard({ id }) {
+  const { selectedConversation } = useConversations()
   return (
     <div className='d-flex' style={{ height: '100vh' }}>
       <Sidebar id={id} />
