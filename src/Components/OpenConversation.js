9b0c55312ef4a5207880c36471f46ca11f41bad3
kandarp43 <kandarpdangi@gmail.com>
2021-06-09 23:47:14 +0530

    init

--- src/Components/OpenConversation.js
+++ src/Components/OpenConversation.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Form, InputGroup } from 'react-bootstrap'
 
 function OpenConversation() {
   return (
     <div className='d-flex flex-column flex-grow-1'>
       <div className='flex-grow-1 overflow-auto'></div>
+      <Form>
+        <Form.Group>
+          <InputGroup>
+        <Form.Control as='textarea' required value={text}
+          </InputGroup>
+        </Form.Group>
+      </Form>
     </div>
   )
 }
