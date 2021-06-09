d693066fb81b2f691f19d8e66e19dc2774ccc2d9
kandarp43 <kandarpdangi@gmail.com>
2021-06-10 00:01:12 +0530

    init

--- src/Components/OpenConversation.js
+++ src/Components/OpenConversation.js
@@ -1,21 +1,25 @@
 import React, { useState } from 'react'
-import { Form, InputGroup } from 'react-bootstrap'
+import { Button, Form, InputGroup } from 'react-bootstrap'
 
 function OpenConversation() {
-  const [text, setText] = useState()
+  const [text, setText] = useState('')
   return (
     <div className='d-flex flex-column flex-grow-1'>
       <div className='flex-grow-1 overflow-auto'></div>
       <Form>
-        <Form.Group>
+        <Form.Group className='m-2'>
           <InputGroup>
             <Form.Control
               as='textarea'
               required
               value={text}
               onChange={(e) => setText(e.target.value)}
+              style={{ height: '40px', resize: 'none' }}
             />
           </InputGroup>
+          <InputGroup.Append>
+            <Button type='submit'>Send</Button>
+          </InputGroup.Append>
         </Form.Group>
       </Form>
     </div>
