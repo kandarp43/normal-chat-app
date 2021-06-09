03ac1774d7cca20ab56cedd418103a6b74ba3b1d
kandarp43 <kandarpdangi@gmail.com>
2021-06-09 20:14:52 +0530

    init

--- src/Components/OpenConversation.js
+++ src/Components/OpenConversation.js
@@ -3,9 +3,7 @@ import React from 'react'
 function OpenConversation() {
   return (
     <div className='d-flex flex-column flex-grow-1'>
-      <div className='flex-grow-1'>
-          
-      </div>
+      <div className='flex-grow-1 overflow-auto'></div>
     </div>
   )
 }
