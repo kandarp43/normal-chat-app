ef5e81643253f920370811950ded364cb3886733
kandarp43 <kandarpdangi@gmail.com>
2021-06-09 20:07:51 +0530

    init

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import OpenConversation from './OpenConversation'
 import Sidebar from './Sidebar'
 
 function Dashboard({ id }) {
   return (
     <div className='d-flex' style={{ height: '100vh' }}>
       <Sidebar id={id} />
+      <OpenConversation />
     </div>
   )
 }
---
+++ src/Components/OpenConversation.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function OpenConversation() {
+  return <div></div>
+}
+
+export default OpenConversation
