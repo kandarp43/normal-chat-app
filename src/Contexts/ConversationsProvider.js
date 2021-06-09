b753e3809c6914f812ab540fdd1fb00d3d864c18
kandarp43 <kandarpdangi@gmail.com>
2021-06-09 09:37:58 +0530

    init

--- src/Contexts/ConversationsProvider.js
+++ src/Contexts/ConversationsProvider.js
@@ -1,5 +1,6 @@
 import React, { createContext, useContext } from 'react'
 import useLocalstorage from '../Hooks/useLocalstorage'
+import { useContacts } from './ContactsProvider'
 
 const conversationContext = createContext()
 
@@ -10,14 +11,20 @@ export function useConversations() {
 export function ConversationsProvider({ children }) {
   const [conversations, setConversations] = useLocalstorage('conversations', [])
 
-  const createConversation = (recipient) => {
+  const { contacts } = useContacts()
+
+  const createConversation = (recipients) => {
     setConversations((prevConversation) => {
-      return [...prevConversation, { recipient, messages: [] }]
+      return [...prevConversation, { recipients, messages: [] }]
     })
   }
 
-  const formattedConversations = conversations.map(conversation=>{
-    const recipient = conversations
+  const formattedConversations = conversations.map((conversation) => {
+    const recipients = conversations.recipients.map((recipient) => {
+      const contact = contacts.find((contact) => {
+        return contact.id === recipient
+      })
+    })
   })
   return (
     <conversationContext.Provider value={{ conversations, createConversation }}>
