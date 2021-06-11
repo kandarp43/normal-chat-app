c4644814e2f79a59a3e8449d7ab424dcd4a89bd9
kandarp43 <kandarpdangi@gmail.com>
2021-06-11 13:07:13 +0530

    init

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import Dashboard from './Components/Dashboard'
 import Login from './Components/Login'
 import { ContactsProvider } from './Contexts/ContactsProvider'
 import { ConversationsProvider } from './Contexts/ConversationsProvider'
+import { SocketProvider } from './Contexts/SocketProvider'
 import useLocalstorage from './Hooks/useLocalstorage'
 
 function App() {
   const [id, setId] = useLocalstorage('id')
 
   const dashboard = (
-    <ContactsProvider>
-      <ConversationsProvider id={id}>
-        <Dashboard id={id} />
-      </ConversationsProvider>
-    </ContactsProvider>
+    <SocketProvider id={id}>
+      <ContactsProvider>
+        <ConversationsProvider id={id}>
+          <Dashboard id={id} />
+        </ConversationsProvider>
+      </ContactsProvider>
+    </SocketProvider>
   )
   return id ? dashboard : <Login onIdSubmit={setId} />
 }
